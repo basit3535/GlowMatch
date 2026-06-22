a29757b50149c823fb8284736516cba5bbc11b0d
Abdul Basit <64415425+basit3535@users.noreply.github.com>
2026-06-22 14:59:07 +0500

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Interfaces\CustomPageInterface;
 use App\Repositories\CustomPageRepository;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        if (env('APP_ENV') !== 'local') {
+               URL::forceScheme('https');
+        }
+
         $this->app->bind( CustomPageInterface::class,CustomPageRepository::class );
     }
 
